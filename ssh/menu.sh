1f65179b396daecebabb0ffdf260705dfc7f1100
donaya365 <82857286+donaya365@users.noreply.github.com>
2022-07-07 20:24:57 +0700

    Add files via upload

---
+++ ssh/menu.sh
@@ -0,0 +1,336 @@
+#!/bin/bash
+clear
+m="\033[0;1;36m"
+y="\033[0;1;37m"
+yy="\033[0;1;32m"
+yl="\033[0;1;33m"
+wh="\033[0m"
+color1='\e[031;1m'
+color2='\e[34;1m'
+color3='\e[0m'
+DF='\e[39m'
+Bold='\e[1m'
+Blink='\e[5m'
+yell='\e[33m'
+red='\e[1;31m'
+green='\e[1;32m'
+blue='\e[1;34m'
+PURPLE='\e[1;95m'
+CYAN='\e[1;36m'
+Lred='\e[1;91m'
+Lgreen='\e[92m'
+Lyellow='\e[93m'
+white='\e[1;37m'
+NC='\e[0m'
+
+echo -e "  $CYAN ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+echo -e "$y          Telegram : https://t.me/zoldyckkkkkk $wh"
+echo -e "$y        PREMIUM AUTO SCRIPT BY ENVY VPN STORE $wh"
+echo -e "  $CYAN ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+echo ""
+echo -e "  $CYAN ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+echo -e "   \E[41;1;39m                ⇱ SSH & OpenVPN ⇲                \E[0m"
+echo -e "  $CYAN ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━$NC"
+echo -e "   [${green}01${NC}]${color1} •$white Create SSH & OpenVPN Account"
+echo -e "   [${green}02${NC}]${color1} •$white Generate SSH & OpenVPN Trial Account"
+echo -e "   [${green}03${NC}]${color1} •$white Extending SSH & OpenVPN Account Active Life"
+echo -e "   [${green}04${NC}]${color1} •$white Check User Login SSH & OpenVPN"
+echo -e "   [${green}05${NC}]${color1} •$white Daftar Member SSH & OpenVPN"
+echo -e "   [${green}06${NC}]${color1} •$white Delete SSH & OpenVpn Account"
+echo -e "   [${green}07${NC}]${color1} •$white Delete User Expired SSH & OpenVPN"
+echo -e "   [${green}08${NC}]${color1} •$white Set up Autokill SSH"
+echo -e "   [${green}09${NC}]${color1} •$white Displays Users Who Do Multi Login SSH"
+echo -e "   [${green}10${NC}]${color1} •$white Restart All Service"
+echo -e ""
+echo -e "  $CYAN ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+echo -e "   \E[41;1;39m                     ⇱ L2TP ⇲                    \E[0m"
+echo -e "  $CYAN ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━$NC"
+echo -e "   [${green}11${NC}]${color1} •$white Create Account L2TP"
+echo -e "   [${green}12${NC}]${color1} •$white Delete Account L2TP"
+echo -e "   [${green}13${NC}]${color1} •$white Extending Account L2TP Active Life"
+echo -e ""
+echo -e "  $CYAN ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+echo -e "   \E[41;1;39m                  ⇱ PPTP ⇲                       \E[0m"
+echo -e "  $CYAN ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━$NC"
+echo -e "   [${green}14${NC}]${color1} •$white Create Account PPTP"
+echo -e "   [${green}15${NC}]${color1} •$white Delete Account PPTP"
+echo -e "   [${green}16${NC}]${color1} •$white Extending Account PPTP Active Life"
+echo -e ""
+echo -e "  $CYAN ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+echo -e "   \E[41;1;39m                  ⇱ SSTP ⇲                       \E[0m"
+echo -e "  $CYAN ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━$NC"
+echo -e "   [${green}17${NC}]${color1} •$white Create Account SSTP"
+echo -e "   [${green}18${NC}]${color1} •$white Delete Account SSTP"
+echo -e "   [${green}19${NC}]${color1} •$white Extending Account SSTP Active Life"
+echo -e "   [${green}20${NC}]${color1} •$white Check User Login SSTP"
+echo -e ""
+echo -e "  $CYAN ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+echo -e "   \E[41;1;39m                  ⇱ WIREGUARD ⇲                  \E[0m"
+echo -e "  $CYAN ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━$NC"
+echo -e "   [${green}21${NC}]${color1} •$white Create Account Wireguard"
+echo -e "   [${green}22${NC}]${color1} •$white Delete Account Wireguard"
+echo -e "   [${green}23${NC}]${color1} •$white Extending Account Wireguard Active Life"
+echo -e ""
+echo -e "  $CYAN ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+echo -e "   \E[41;1;39m                  ⇱ SHADOWSOCKS ⇲                \E[0m"
+echo -e "  $CYAN ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━$NC"
+echo -e "   [${green}24${NC}]${color1} •$white Create Account Shadowsocks"
+echo -e "   [${green}25${NC}]${color1} •$white Delete Account Shadowsocks"
+echo -e "   [${green}26${NC}]${color1} •$white Extending Account Shadowsocks Active Life"
+echo -e "   [${green}27${NC}]${color1} •$white Check User Login Shadowsocks"
+echo -e ""
+echo -e "  $CYAN ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+echo -e "   \E[41;1;39m                  ⇱ SHADOWSOCKSR ⇲               \E[0m"
+echo -e "  $CYAN ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━$NC"
+echo -e "   [${green}28${NC}]${color1} •$white Create Account SSR"
+echo -e "   [${green}29${NC}]${color1} •$white Delete Account SSR"
+echo -e "   [${green}30${NC}]${color1} •$white Extending Account SSR Active Life"
+echo -e "   [${green}31${NC}]${color1} •$white Show Other SSR Menu"
+echo -e ""
+echo -e "  $CYAN ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+echo -e "   \E[41;1;39m                  ⇱ XRAY / VMESS ⇲               \E[0m"
+echo -e "  $CYAN ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━$NC"
+echo -e "   [${green}32${NC}]${color1} •$white Create Account XRAYS Vmess Websocket"
+echo -e "   [${green}33${NC}]${color1} •$white Delete Account XRAYS Vmess Websocket"
+echo -e "   [${green}34${NC}]${color1} •$white Extending Account XRAYS Vmess Active Life"
+echo -e "   [${green}35${NC}]${color1} •$white Check User Login XRAYS Vmess"
+echo -e "   [${green}36${NC}]${color1} •$white Renew Certificate XRAYS Account"
+echo -e ""
+echo -e "  $CYAN ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+echo -e "   \E[41;1;39m                  ⇱ XRAY / VLESS ⇲               \E[0m"
+echo -e "  $CYAN ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━$NC"
+echo -e "   [${green}37${NC}]${color1} •$white Create Account XRAYS Vless Websocket"
+echo -e "   [${green}38${NC}]${color1} •$white Delete Account XRAYS Vless Websocket"
+echo -e "   [${green}39${NC}]${color1} •$white Extending Account XRAYS Vless Active Life"
+echo -e "   [${green}40${NC}]${color1} •$white Check User Login XRAYS Vless"
+echo -e ""
+echo -e "  $CYAN ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+echo -e "   \E[41;1;39m                  ⇱ XRAY / TROJAN ⇲              \E[0m"
+echo -e "  $CYAN ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━$NC"
+echo -e "   [${green}41${NC}]${color1} •$white Create Account XRAYS Trojan"
+echo -e "   [${green}42${NC}]${color1} •$white Delete Account XRAYS Trojan"
+echo -e "   [${green}43${NC}]${color1} •$white Extending Account XRAYS Trojan Active Life"
+echo -e "   [${green}44${NC}]${color1} •$white Check User Login XRAYS Trojan"
+echo -e ""
+echo -e "  $CYAN ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+echo -e "   \E[41;1;39m                  ⇱ TROJAN GO ⇲                  \E[0m"
+echo -e "  $CYAN ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━$NC"
+echo -e "   [${green}45${NC}]${color1} •$white Create Account Trojan Go"
+echo -e "   [${green}46${NC}]${color1} •$white Delete Account Trojan Go"
+echo -e "   [${green}47${NC}]${color1} •$white Extending Account Trojan Go Active Life"
+echo -e "   [${green}48${NC}]${color1} •$white Check User Login Trojan Go"
+echo ""
+echo -e "  $CYAN ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+echo -e "   \E[41;1;39m                     ⇱ SYSTEM ⇲                  \E[0m"
+echo -e "  $CYAN ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━$NC"
+echo -e "   [${green}49${NC}]${color1} •$white Add Or Change Subdomain Host For VPS"
+echo -e "   [${green}50${NC}]${color1} •$white Change Port Of Some Service"
+echo -e "   [${green}51${NC}]${color1} •$white Autobackup Data VPS"
+echo -e "   [${green}52${NC}]${color1} •$white Backup Data VPS"
+echo -e "   [${green}53${NC}]${color1} •$white Restore Data VPS"
+echo -e "   [${green}54${NC}]${color1} •$white Webmin Menu"
+echo -e "   [${green}55${NC}]${color1} •$white Limit Bandwith Speed Server"
+echo -e "   [${green}56${NC}]${color1} •$white Check Usage of VPS Ram"
+echo -e "   [${green}57${NC}]${color1} •$white Reboot VPS"
+echo -e "   [${green}58${NC}]${color1} •$white Speedtest VPS"
+echo -e "   [${green}59${NC}]${color1} •$white Displaying System Information"
+echo -e "   [${green}60${NC}]${color1} •$white Info Script Auto Install"
+echo -e "   [${green}61${NC}]${color1} •$white Status Running Service"
+echo -e "  $CYAN ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+echo -e "   [${green}00${NC}]${color1} •$white BACK TO EXIT MENU \033[1;32m<\033[1;33m<\033[1;31m<\033[1;31m $NC"
+echo -e "  $CYAN ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ $NC"
+echo -e ""
+    read -p "Select From Options [ 1 - 61 ] : " menu
+echo -e ""
+echo -e ""
+case $menu in
+1 | 01)
+addssh
+;;
+2 | 02)
+trialssh
+;;
+3 | 03)
+renewssh
+;;
+4 | 04)
+cekssh
+;;
+5 | 05)
+member
+;;
+6 | 06)
+delssh
+;;
+7 | 07)
+delexp
+;;
+8 | 08)
+autokill
+;;
+9 | 09)
+ceklim
+;;
+10)
+restart
+;;
+11)
+addl2tp
+;;
+12)
+dell2tp
+;;
+13)
+renewl2tp
+;;
+14)
+addpptp
+;;
+15)
+delpptp
+;;
+16)
+renewpptp
+;;
+17)
+addsstp
+;;
+18)
+delsstp
+;;
+19)
+renewsstp
+;;
+20)
+ceksstp
+;;
+21)
+addwg
+;;
+22)
+delwg
+;;
+23)
+renewwg
+;;
+24)
+addss
+;;
+25)
+delss
+;;
+26)
+renewss
+;;
+27)
+cekss
+;;
+28)
+addssr
+;;
+29)
+delssr
+;;
+30)
+renewssr
+;;
+31)
+ssr
+;;
+32)
+addv2ray
+;;
+33)
+delv2ray
+;;
+34)
+renewv2ray
+;;
+35)
+cekv2ray
+;;
+36)
+certv2ray
+;;
+37)
+addvless
+;;
+38)
+delvless
+;;
+39)
+renewvless
+;;
+40)
+cekvless
+;;
+41)
+addtrojan
+;;
+42)
+deltrojan
+;;
+43)
+renewtrojan
+;;
+44)
+cektrojan
+;;
+45)
+addtrgo
+;;
+46)
+deltrgo
+;;
+47)
+renewtrgo
+;;
+48)
+cektrgo
+;;
+49)
+addhost
+;;
+50)
+changeport
+;;
+51)
+autobackup
+;;
+52)
+backup
+;;
+53)
+restore
+;;
+54)
+wbmn
+;;
+55)
+limitspeed
+;;
+56)
+ram
+;;
+57)
+reboot
+;;
+58)
+speedtest
+;;
+59)
+info
+;;
+60)
+about
+;;
+61)
+running
+;;
+0 | 00)
+exit
+;;
+*)
+menu
+;;
+esac
---
+++ ssh/ssh-vpn.sh
@@ -0,0 +1,552 @@
+#!/bin/bash
+# By colongvpn
+# My Telegram : https://t.me/zoldyckkkkkk
+# ==========================================
+# Color
+RED='\033[0;31m'
+NC='\033[0m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHT='\033[0;37m'
+# ==========================================
+# Getting
+MYIP=$(wget -qO- ipinfo.io/ip);
+echo "Checking VPS"
+IZIN=$( curl http://raw.githubusercontent.com/donaya365/perizinan/main/ipvps.txt | grep $MYIP )
+if [ $MYIP = $IZIN ]; then
+echo -e "${NC}${GREEN}Permission Accepted...${NC}"
+else
+echo -e "${NC}${RED}Permission Denied!${NC}";
+echo -e "${NC}${LIGHT}Please Contact Admin!!"
+echo -e "${NC}${LIGHT}Telegram : https://t.me/zoldyckkkkkk"
+exit 0
+fi
+# ==================================================
+# Link Hosting Kalian
+colongvpn="raw.githubusercontent.com/donaya365/scriptvps/main/ssh"
+
+# Link Hosting Kalian Untuk Xray
+colongvpnn="raw.githubusercontent.com/donaya365/scriptvps/main/xray"
+
+# Link Hosting Kalian Untuk Trojan Go
+colongvpnnn="raw.githubusercontent.com/donaya365/scriptvps/main/trojango"
+
+# Link Hosting Kalian Untuk Stunnel5
+colongvpnnnn="raw.githubusercontent.com/donaya365/scriptvps/main/stunnel5"
+
+# initializing var
+export DEBIAN_FRONTEND=noninteractive
+MYIP=$(wget -qO- ipinfo.io/ip);
+MYIP2="s/xxxxxxxxx/$MYIP/g";
+NET=$(ip -o $ANU -4 route show to default | awk '{print $5}');
+source /etc/os-release
+ver=$VERSION_ID
+
+#detail nama perusahaan
+country=ID
+state=Indonesia
+locality=Indonesia
+organization=colongvpn
+organizationalunit=colongvpn
+commonname=colongvpn
+email=colongvpnstore@gmail.com
+
+# simple password minimal
+wget -O /etc/pam.d/common-password "https://${colongvpn}/password"
+chmod +x /etc/pam.d/common-password
+
+# go to root
+cd
+
+# Edit file /etc/systemd/system/rc-local.service
+cat > /etc/systemd/system/rc-local.service <<-END
+[Unit]
+Description=/etc/rc.local
+ConditionPathExists=/etc/rc.local
+[Service]
+Type=forking
+ExecStart=/etc/rc.local start
+TimeoutSec=0
+StandardOutput=tty
+RemainAfterExit=yes
+SysVStartPriority=99
+[Install]
+WantedBy=multi-user.target
+END
+
+# nano /etc/rc.local
+cat > /etc/rc.local <<-END
+#!/bin/sh -e
+# rc.local
+# By default this script does nothing.
+exit 0
+END
+
+# Ubah izin akses
+chmod +x /etc/rc.local
+
+# enable rc local
+systemctl enable rc-local
+systemctl start rc-local.service
+
+# disable ipv6
+echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6
+sed -i '$ i\echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6' /etc/rc.local
+
+#update
+apt update -y
+apt upgrade -y
+apt dist-upgrade -y
+apt-get remove --purge ufw firewalld -y
+apt-get remove --purge exim4 -y
+
+# install wget and curl
+apt -y install wget curl
+
+# Install Requirements Tools
+apt install ruby -y
+apt install python -y
+apt install make -y
+apt install cmake -y
+apt install coreutils -y
+apt install rsyslog -y
+apt install net-tools -y
+apt install zip -y
+apt install unzip -y
+apt install nano -y
+apt install sed -y
+apt install gnupg -y
+apt install gnupg1 -y
+apt install bc -y
+apt install jq -y
+apt install apt-transport-https -y
+apt install build-essential -y
+apt install dirmngr -y
+apt install libxml-parser-perl -y
+apt install neofetch -y
+apt install git -y
+apt install lsof -y
+apt install libsqlite3-dev -y
+apt install libz-dev -y
+apt install gcc -y
+apt install g++ -y
+apt install libreadline-dev -y
+apt install zlib1g-dev -y
+apt install libssl-dev -y
+apt install libssl1.0-dev -y
+apt install dos2unix -y
+
+# set time GMT +7
+ln -fs /usr/share/zoneinfo/Asia/Jakarta /etc/localtime
+
+# set locale
+sed -i 's/AcceptEnv/#AcceptEnv/g' /etc/ssh/sshd_config
+
+# install
+apt-get --reinstall --fix-missing install -y bzip2 gzip coreutils wget screen rsyslog iftop htop net-tools zip unzip wget net-tools curl nano sed screen gnupg gnupg1 bc apt-transport-https build-essential dirmngr libxml-parser-perl neofetch git lsof
+echo "clear" >> .profile
+echo "neofetch" >> .profile
+
+# install webserver
+apt -y install nginx php php-fpm php-cli php-mysql libxml-parser-perl
+rm /etc/nginx/sites-enabled/default
+rm /etc/nginx/sites-available/default
+curl https://${colongvpn}/nginx.conf > /etc/nginx/nginx.conf
+curl https://${colongvpn}/vps.conf > /etc/nginx/conf.d/vps.conf
+sed -i 's/listen = \/var\/run\/php-fpm.sock/listen = 127.0.0.1:9000/g' /etc/php/fpm/pool.d/www.conf
+useradd -m vps;
+mkdir -p /home/vps/public_html
+echo "<?php phpinfo() ?>" > /home/vps/public_html/info.php
+chown -R www-data:www-data /home/vps/public_html
+chmod -R g+rw /home/vps/public_html
+cd /home/vps/public_html
+wget -O /home/vps/public_html/index.html "https://${colongvpn}/index.html1"
+/etc/init.d/nginx restart
+cd
+
+# install badvpn
+cd
+wget -O /usr/bin/badvpn-udpgw "https://${colongvpn}/badvpn-udpgw64"
+chmod +x /usr/bin/badvpn-udpgw
+sed -i '$ i\screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7100 --max-clients 500' /etc/rc.local
+sed -i '$ i\screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7200 --max-clients 500' /etc/rc.local
+sed -i '$ i\screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300 --max-clients 500' /etc/rc.local
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7100 --max-clients 500
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7200 --max-clients 500
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300 --max-clients 500
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7400 --max-clients 500
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7500 --max-clients 500
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7600 --max-clients 500
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7700 --max-clients 500
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7800 --max-clients 500
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7900 --max-clients 500
+
+# setting port ssh
+sed -i 's/Port 22/Port 22/g' /etc/ssh/sshd_config
+
+# install dropbear
+apt -y install dropbear
+sed -i 's/NO_START=1/NO_START=0/g' /etc/default/dropbear
+sed -i 's/DROPBEAR_PORT=22/DROPBEAR_PORT=143/g' /etc/default/dropbear
+sed -i 's/DROPBEAR_EXTRA_ARGS=/DROPBEAR_EXTRA_ARGS="-p 109"/g' /etc/default/dropbear
+echo "/bin/false" >> /etc/shells
+echo "/usr/sbin/nologin" >> /etc/shells
+/etc/init.d/dropbear restart
+
+# install squid
+cd
+apt -y install squid3
+wget -O /etc/squid/squid.conf "https://${colongvpn}/squid3.conf"
+sed -i $MYIP2 /etc/squid/squid.conf
+
+# Install SSLH
+apt -y install sslh
+rm -f /etc/default/sslh
+
+# Settings SSLH
+cat > /etc/default/sslh <<-END
+# Default options for sslh initscript
+# sourced by /etc/init.d/sslh
+
+# Disabled by default, to force yourself
+# to read the configuration:
+# - /usr/share/doc/sslh/README.Debian (quick start)
+# - /usr/share/doc/sslh/README, at "Configuration" section
+# - sslh(8) via "man sslh" for more configuration details.
+# Once configuration ready, you *must* set RUN to yes here
+# and try to start sslh (standalone mode only)
+
+RUN=yes
+
+# binary to use: forked (sslh) or single-thread (sslh-select) version
+# systemd users: don't forget to modify /lib/systemd/system/sslh.service
+DAEMON=/usr/sbin/sslh
+
+DAEMON_OPTS="--user sslh --listen 0.0.0.0:443 --ssl 127.0.0.1:777 --ssh 127.0.0.1:109 --openvpn 127.0.0.1:1194 --http 127.0.0.1:8880 --pidfile /var/run/sslh/sslh.pid -n"
+
+END
+
+# Restart Service SSLH
+service sslh restart
+systemctl restart sslh
+/etc/init.d/sslh restart
+/etc/init.d/sslh status
+/etc/init.d/sslh restart
+
+# setting vnstat
+apt -y install vnstat
+/etc/init.d/vnstat restart
+apt -y install libsqlite3-dev
+wget https://humdi.net/vnstat/vnstat-2.6.tar.gz
+tar zxvf vnstat-2.6.tar.gz
+cd vnstat-2.6
+./configure --prefix=/usr --sysconfdir=/etc && make && make install
+cd
+vnstat -u -i $NET
+sed -i 's/Interface "'""eth0""'"/Interface "'""$NET""'"/g' /etc/vnstat.conf
+chown vnstat:vnstat /var/lib/vnstat -R
+systemctl enable vnstat
+/etc/init.d/vnstat restart
+rm -f /root/vnstat-2.6.tar.gz
+rm -rf /root/vnstat-2.6
+
+mkdir -p /usr/local/colongvpn
+mkdir -p /etc/colongvpn
+
+# install stunnel 5 
+cd /root/
+wget -q -O stunnel5.zip "https://${colongvpnnnn}/stunnel5.zip"
+unzip -o stunnel5.zip
+cd /root/stunnel
+chmod +x configure
+./configure
+make
+make install
+cd /root
+rm -r -f stunnel
+rm -f stunnel5.zip
+mkdir -p /etc/stunnel5
+chmod 644 /etc/stunnel5
+
+# Download Config Stunnel5
+cat > /etc/stunnel5/stunnel5.conf <<-END
+cert = /etc/stunnel5/stunnel5.pem
+client = no
+socket = a:SO_REUSEADDR=1
+socket = l:TCP_NODELAY=1
+socket = r:TCP_NODELAY=1
+
+[dropbear]
+accept = 445
+connect = 127.0.0.1:109
+
+[openssh]
+accept = 777
+connect = 127.0.0.1:443
+
+[openvpn]
+accept = 990
+connect = 127.0.0.1:1194
+
+END
+
+# make a certificate
+openssl genrsa -out key.pem 2048
+openssl req -new -x509 -key key.pem -out cert.pem -days 1095 \
+-subj "/C=$country/ST=$state/L=$locality/O=$organization/OU=$organizationalunit/CN=$commonname/emailAddress=$email"
+cat key.pem cert.pem >> /etc/stunnel5/stunnel5.pem
+
+# Service Stunnel5 systemctl restart stunnel5
+cat > /etc/systemd/system/stunnel5.service << END
+[Unit]
+Description=Stunnel5 Service
+Documentation=https://stunnel.org
+Documentation=https://github.com/colongvpn
+After=syslog.target network-online.target
+
+[Service]
+ExecStart=/usr/local/colongvpn/stunnel5 /etc/stunnel5/stunnel5.conf
+Type=forking
+
+[Install]
+WantedBy=multi-user.target
+END
+
+# Service Stunnel5 /etc/init.d/stunnel5
+wget -q -O /etc/init.d/stunnel5 "https://${colongvpnnnn}/stunnel5.init"
+
+# Ubah Izin Akses
+chmod 600 /etc/stunnel5/stunnel5.pem
+chmod +x /etc/init.d/stunnel5
+cp /usr/local/bin/stunnel /usr/local/colongvpn/stunnel5
+
+# Remove File
+rm -r -f /usr/local/share/doc/stunnel/
+rm -r -f /usr/local/etc/stunnel/
+rm -f /usr/local/bin/stunnel
+rm -f /usr/local/bin/stunnel3
+rm -f /usr/local/bin/stunnel4
+rm -f /usr/local/bin/stunnel5
+
+# Restart Stunnel 5
+systemctl stop stunnel5
+systemctl enable stunnel5
+systemctl start stunnel5
+systemctl restart stunnel5
+/etc/init.d/stunnel5 restart
+/etc/init.d/stunnel5 status
+/etc/init.d/stunnel5 restart
+
+#OpenVPN
+wget https://${colongvpn}/vpn.sh &&  chmod +x vpn.sh && ./vpn.sh
+
+echo; echo 'Installing DOS-Deflate 0.6'; echo
+echo; echo -n 'Downloading source files...'
+apt install -y dnsutils tcpdump dsniff grepcidr
+wget -qO ddos.zip "https://${colongvpn}/ddos-deflate.zip" > /dev/null 2>&1
+unzip ddos.zip
+cd ddos-deflate
+chmod +x install.sh
+./install.sh
+
+# install fail2ban
+apt -y install fail2ban
+
+# Instal DDOS Flate
+if [ -d '/usr/local/ddos' ]; then
+	echo; echo; echo "Please un-install the previous version first"
+	exit 0
+else
+	mkdir /usr/local/ddos
+fi
+clear
+echo; echo 'Installing DOS-Deflate 0.6'; echo
+echo; echo -n 'Downloading source files...'
+wget -q -O /usr/local/ddos/ddos.conf http://www.inetbase.com/scripts/ddos/ddos.conf
+echo -n '.'
+wget -q -O /usr/local/ddos/LICENSE http://www.inetbase.com/scripts/ddos/LICENSE
+echo -n '.'
+wget -q -O /usr/local/ddos/ignore.ip.list http://www.inetbase.com/scripts/ddos/ignore.ip.list
+echo -n '.'
+wget -q -O /usr/local/ddos/ddos.sh http://www.inetbase.com/scripts/ddos/ddos.sh
+chmod 0755 /usr/local/ddos/ddos.sh
+cp -s /usr/local/ddos/ddos.sh /usr/local/sbin/ddos
+echo '...done'
+echo; echo -n 'Creating cron to run script every minute.....(Default setting)'
+/usr/local/ddos/ddos.sh --cron > /dev/null 2>&1
+echo '.....done'
+echo; echo 'Installation has completed.'
+echo 'Config file is at /usr/local/ddos/ddos.conf'
+echo 'Please send in your comments and/or suggestions to zaf@vsnl.com'
+
+# banner /etc/issue.net
+echo "Banner /etc/issue.net" >>/etc/ssh/sshd_config
+sed -i 's@DROPBEAR_BANNER=""@DROPBEAR_BANNER="/etc/issue.net"@g' /etc/default/dropbear
+
+# Install BBR
+wget https://${colongvpn}/bbr.sh && chmod +x bbr.sh && ./bbr.sh
+
+# Ganti Banner
+wget -O /etc/issue.net "https://${colongvpn}/issue.net"
+
+# blockir torrent
+iptables -A FORWARD -m string --string "get_peers" --algo bm -j DROP
+iptables -A FORWARD -m string --string "announce_peer" --algo bm -j DROP
+iptables -A FORWARD -m string --string "find_node" --algo bm -j DROP
+iptables -A FORWARD -m string --algo bm --string "BitTorrent" -j DROP
+iptables -A FORWARD -m string --algo bm --string "BitTorrent protocol" -j DROP
+iptables -A FORWARD -m string --algo bm --string "peer_id=" -j DROP
+iptables -A FORWARD -m string --algo bm --string ".torrent" -j DROP
+iptables -A FORWARD -m string --algo bm --string "announce.php?passkey=" -j DROP
+iptables -A FORWARD -m string --algo bm --string "torrent" -j DROP
+iptables -A FORWARD -m string --algo bm --string "announce" -j DROP
+iptables -A FORWARD -m string --algo bm --string "info_hash" -j DROP
+iptables-save > /etc/iptables.up.rules
+iptables-restore -t < /etc/iptables.up.rules
+netfilter-persistent save
+netfilter-persistent reload
+
+# download script
+cd /usr/bin
+wget -O addhost "https://${colongvpn}/addhost.sh"
+wget -O about "https://${colongvpn}/about.sh"
+wget -O menu "https://${colongvpn}/menu.sh"
+wget -O addssh "https://${colongvpn}/addssh.sh"
+wget -O trialssh "https://${colongvpn}/trialssh.sh"
+wget -O delssh "https://${colongvpn}/delssh.sh"
+wget -O member "https://${colongvpn}/member.sh"
+wget -O delexp "https://${colongvpn}/delexp.sh"
+wget -O cekssh "https://${colongvpn}/cekssh.sh"
+wget -O restart "https://${colongvpn}/restart.sh"
+wget -O speedtest "https://${colongvpn}/speedtest_cli.py"
+wget -O info "https://${colongvpn}/info.sh"
+wget -O ram "https://${colongvpn}/ram.sh"
+wget -O renewssh "https://${colongvpn}/renewssh.sh"
+wget -O autokill "https://${colongvpn}/autokill.sh"
+wget -O ceklim "https://${colongvpn}/ceklim.sh"
+wget -O tendang "https://${colongvpn}/tendang.sh"
+wget -O clearlog "https://${colongvpn}/clearlog.sh"
+wget -O changeport "https://${colongvpn}/changeport.sh"
+wget -O portovpn "https://${colongvpn}/portovpn.sh"
+wget -O portwg "https://${colongvpn}/portwg.sh"
+wget -O porttrojan "https://${colongvpn}/porttrojan.sh"
+wget -O portsstp "https://${colongvpn}/portsstp.sh"
+wget -O portsquid "https://${colongvpn}/portsquid.sh"
+wget -O portv2ray "https://${colongvpn}/portv2ray.sh"
+wget -O portvless "https://${colongvpn}/portvless.sh"
+wget -O wbmn "https://${colongvpn}/webmin.sh"
+wget -O xp "https://${colongvpn}/xp.sh"
+wget -O running "https://${colongvpn}/running.sh"
+wget -O swapkvm "https://${colongvpn}/swapkvm.sh"
+wget -O addv2ray "https://${colongvpnn}/addv2ray.sh"
+wget -O addvless "https://${colongvpnn}/addvless.sh"
+wget -O addtrojan "https://${colongvpnn}/addtrojan.sh"
+wget -O delv2ray "https://${colongvpnn}/delv2ray.sh"
+wget -O delvless "https://${colongvpnn}/delvless.sh"
+wget -O deltrojan "https://${colongvpnn}/deltrojan.sh"
+wget -O cekv2ray "https://${colongvpnn}/cekv2ray.sh"
+wget -O cekvless "https://${colongvpnn}/cekvless.sh"
+wget -O cektrojan "https://${colongvpnn}/cektrojan.sh"
+wget -O renewv2ray "https://${colongvpnn}/renewv2ray.sh"
+wget -O renewvless "https://${colongvpnn}/renewvless.sh"
+wget -O renewtrojan "https://${colongvpnn}/renewtrojan.sh"
+wget -O certv2ray "https://${colongvpnn}/certv2ray.sh"
+wget -O addtrgo "https://${colongvpnnn}/addtrgo.sh"
+wget -O deltrgo "https://${colongvpnnn}/deltrgo.sh"
+wget -O renewtrgo "https://${colongvpnnn}/renewtrgo.sh"
+wget -O cektrgo "https://${colongvpnnn}/cektrgo.sh"
+chmod +x addhost
+chmod +x menu
+chmod +x addssh
+chmod +x trialssh
+chmod +x delssh
+chmod +x member
+chmod +x delexp
+chmod +x cekssh
+chmod +x restart
+chmod +x speedtest
+chmod +x info
+chmod +x about
+chmod +x autokill
+chmod +x tendang
+chmod +x ceklim
+chmod +x ram
+chmod +x renewssh
+chmod +x clearlog
+chmod +x changeport
+chmod +x portovpn
+chmod +x portwg
+chmod +x porttrojan
+chmod +x portsstp
+chmod +x portsquid
+chmod +x portv2ray
+chmod +x portvless
+chmod +x wbmn
+chmod +x xp
+chmod +x swapkvm
+chmod +x addv2ray
+chmod +x addvless
+chmod +x addtrojan
+chmod +x delv2ray
+chmod +x delvless
+chmod +x deltrojan
+chmod +x cekv2ray
+chmod +x cekvless
+chmod +x cektrojan
+chmod +x renewv2ray
+chmod +x renewvless
+chmod +x renewtrojan
+chmod +x certv2ray
+chmod +x addtrgo
+chmod +x deltrgo
+chmod +x renewtrgo
+chmod +x cektrgo
+chmod +x running
+echo "0 5 * * * root clearlog && reboot" >> /etc/crontab
+echo "0 0 * * * root xp" >> /etc/crontab
+# remove unnecessary files
+cd
+apt autoclean -y
+apt -y remove --purge unscd
+apt-get -y --purge remove samba*;
+apt-get -y --purge remove apache2*;
+apt-get -y --purge remove bind9*;
+apt-get -y remove sendmail*
+apt autoremove -y
+# finishing
+cd
+chown -R www-data:www-data /home/vps/public_html
+/etc/init.d/nginx restart
+/etc/init.d/openvpn restart
+/etc/init.d/cron restart
+/etc/init.d/ssh restart
+/etc/init.d/dropbear restart
+/etc/init.d/fail2ban restart
+/etc/init.d/sslh restart
+/etc/init.d/stunnel5 restart
+/etc/init.d/vnstat restart
+/etc/init.d/fail2ban restart
+/etc/init.d/squid restart
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7100 --max-clients 500
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7200 --max-clients 500
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300 --max-clients 500
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7400 --max-clients 500
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7500 --max-clients 500
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7600 --max-clients 500
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7700 --max-clients 500
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7800 --max-clients 500
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7900 --max-clients 500
+history -c
+echo "unset HISTFILE" >> /etc/profile
+
+cd
+rm -f /root/key.pem
+rm -f /root/cert.pem
+rm -f /root/ssh-vpn.sh
+rm -f /root/ddos.zip
+
+# finihsing
+clear
