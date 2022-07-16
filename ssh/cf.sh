fd28a0b911605c1ed0a66fcb9c1340113db1da48
donaya365 <82857286+donaya365@users.noreply.github.com>
2022-07-16 20:35:32 +0700

    Update cf.sh
    Signed-off-by: donaya365 <82857286+donaya365@users.noreply.github.com>

--- ssh/cf.sh
+++ ssh/cf.sh
@@ -28,8 +28,8 @@ apt install jq curl -y
 DOMAIN=vip-server.xyz
 sub=$(</dev/urandom tr -dc a-z0-9 | head -c4)
 SUB_DOMAIN=${sub}.vip-server.xyz
-CF_ID=paoandest@gmail.com
-CF_KEY=1d158d0efc4eef787222cefff0b6d20981462
+CF_ID=kidroster55@gmail.com
+CF_KEY=1fa7ccec0343954e2242e221e38a439d20277
 set -euo pipefail
 IP=$(wget -qO- ipinfo.io/ip);
 echo "Updating DNS for ${SUB_DOMAIN}..."
